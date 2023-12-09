c4d38d4bfa6da2d89cb394765935c6e34ba6c62f
Kishan <29030616+pfeux@users.noreply.github.com>
2023-12-10 10:13:55 +1100

    Add files via upload

---
+++ build-td-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+TZ=America/New_York
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+DEBIAN_FRONTEND=noninteractive
+
+apt-get update && apt-get upgrade -y && apt-get autoremove -y && apt-get install make git zlib1g-dev libssl-dev gperf cmake clang-14 libc++-dev libc++abi-dev -y
+
+cd /app
+git clone --recursive https://github.com/tdlib/telegram-bot-api.git
+
+cd /app/telegram-bot-api
+mkdir build
+cd build
+
+CXXFLAGS="-stdlib=libc++" CC=/usr/bin/clang-14 CXX=/usr/bin/clang++-14 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=.. ..
+cmake --build . --target install
