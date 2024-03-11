e86189c3d1247fabeb469bedc2c58f4a5260d6db
Kishan <29030616+pfeux@users.noreply.github.com>
2024-03-11 23:41:14 +1100

    Update build-td-web.sh

--- build-td-web.sh
+++ build-td-web.sh
@@ -6,10 +6,10 @@ DEBIAN_FRONTEND=noninteractive
 apt-get update && apt-get upgrade -y && apt-get autoremove -y && apt-get install make git zlib1g-dev libssl-dev gperf php-cli cmake clang libc++-dev libc++abi-dev sed tar wget python3 python-is-python3 curl -y
 
 cd /app
-git clone https://github.com/tdlib/td.git && git clone https://github.com/emscripten-core/emsdk.git && cd /app/emsdk && git checkout tags/3.1.1
+git clone https://github.com/tdlib/td.git && git clone https://github.com/emscripten-core/emsdk.git && cd /app/emsdk
 
 cd /app/emsdk
-./emsdk install 3.1.1 && ./emsdk activate 3.1.1 && source ./emsdk_env.sh
+./emsdk install latest && ./emsdk activate latest && source ./emsdk_env.sh
 
 cd /app/td/example/web
 sed -i 's/emmake make depend || exit 1/emmake make -j $(nproc) depend || exit 1/g' build-openssl.sh
@@ -18,11 +18,6 @@ sed -i 's/emmake make -j 4 || exit 1/emmake make -j $(nproc) || exit 1/g' build-
 sed -i 's/cmake --build build\/generate --target prepare_cross_compiling || exit 1/cmake --build build\/generate --target prepare_cross_compiling -- -j $(nproc) || exit 1/g' build-tdlib.sh
 sed -i 's/cmake --build build\/wasm --target td_wasm || exit 1/cmake --build build\/wasm --target td_wasm -- -j $(nproc) || exit 1/g' build-tdlib.sh
 sed -i 's/cmake --build build\/asmjs --target td_asmjs || exit 1/cmake --build build\/asmjs --target td_asmjs -- -j $(nproc) || exit 1/g' build-tdlib.sh
-
-# sed -i '/"clean-webpack-plugin": "^2.0.1",/a \ \ "copy-webpack-plugin": "^5.0.5",' /app/td/example/web/tdweb/package.json
-# sed -i 's/CleanWebpackPlugin({})/CleanWebpackPlugin({}),/' /app/td/example/web/tdweb/webpack.config.js
-# sed -i '/const CleanWebpackPlugin = require('"'"'clean-webpack-plugin'"'"');/a const CopyWebpackPlugin = require('"'"'copy-webpack-plugin'"'"');' /app/td/example/web/tdweb/webpack.config.js
-# sed -i '/new CleanWebpackPlugin({}),/a \    new CopyWebpackPlugin([\n      { from: path.resolve(__dirname, "src", "prebuilt"), to: path.resolve(__dirname, "dist", "prebuilt") },\n    ]),' /app/td/example/web/tdweb/webpack.config.js
 
 sed -i '/npm run build || exit 1/a npm pack' build-tdweb.sh
 
