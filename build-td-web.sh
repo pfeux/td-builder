6d306e6afac91d15a2207d7711c5ce61d5270e32
Kishan <29030616+pfeux@users.noreply.github.com>
2024-03-30 01:06:40 +1100

    Update build-td-web.sh

--- build-td-web.sh
+++ build-td-web.sh
@@ -12,12 +12,20 @@ cd /app/emsdk
 ./emsdk install latest && ./emsdk activate latest && source ./emsdk_env.sh
 
 cd /app/td/example/web
+
 sed -i 's/emmake make depend || exit 1/emmake make -j $(nproc) depend || exit 1/g' build-openssl.sh
 sed -i 's/emmake make -j 4 || exit 1/emmake make -j $(nproc) || exit 1/g' build-openssl.sh
 
 sed -i 's/cmake --build build\/generate --target prepare_cross_compiling || exit 1/cmake --build build\/generate --target prepare_cross_compiling -- -j $(nproc) || exit 1/g' build-tdlib.sh
 sed -i 's/cmake --build build\/wasm --target td_wasm || exit 1/cmake --build build\/wasm --target td_wasm -- -j $(nproc) || exit 1/g' build-tdlib.sh
-sed -i 's/cmake --build build\/asmjs --target td_asmjs || exit 1/cmake --build build\/asmjs --target td_asmjs -- -j $(nproc) || exit 1/g' build-tdlib.sh
+# sed -i 's/cmake --build build\/asmjs --target td_asmjs || exit 1/cmake --build build\/asmjs --target td_asmjs -- -j $(nproc) || exit 1/g' build-tdlib.sh
+
+# Removing asmjs building
+sed -i '/console\.log('\''loadTdlibAsmjs'\'');/,/return module;/d' tdweb/src/worker.js
+sed -i '/import td_asmjs_mem_release from '\''\.\/prebuilt\/release\/td_asmjs\.js\.mem'\'';/d' tdweb/src/worker.js
+
+sed -i '/cmake --build build\/asmjs --target td_asmjs || exit 1/d' build-tdlib.sh 
+sed -i 's/cp build\/asmjs\/td_asmjs.js build\/asmjs\/td_asmjs.js.mem $DEST || exit 1/cp build\/asmjs\/td_asmjs.js $DEST || exit 1/' copy-tdlib.sh 
 
 # sed -i '/npm run build || exit 1/a npm pack' build-tdweb.sh
 
@@ -25,6 +33,6 @@ chmod +x build-openssl.sh build-tdlib.sh build-tdweb.sh
 
 ./build-openssl.sh
 ./build-tdlib.sh
-tree -h 
+tree /app/td/example/web/build -h 
 ./copy-tdlib.sh
 ./build-tdweb.sh
