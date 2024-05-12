c9f50d6ad3a57bfbd158e0bda2572465cf15f981
Kishan <29030616+pfeux@users.noreply.github.com>
2024-05-12 21:16:45 +1000

    Update build-td-server.sh

--- build-td-server.sh
+++ build-td-server.sh
@@ -3,7 +3,7 @@ TZ=America/New_York
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 DEBIAN_FRONTEND=noninteractive
 
-apt-get update && apt-get upgrade -y && apt-get autoremove -y && apt-get install make git zlib1g-dev libssl-dev gperf cmake clang-14 libc++-dev libc++abi-dev -y
+apt-get update && apt-get upgrade -y && apt-get autoremove -y && apt-get install make git zlib1g-dev libssl-dev gperf cmake clang libc++-dev libc++abi-dev -y
 
 cd /app
 git clone --recursive https://github.com/tdlib/telegram-bot-api.git
@@ -12,5 +12,5 @@ cd /app/telegram-bot-api
 mkdir build
 cd build
 
-CXXFLAGS="-stdlib=libc++" CC=/usr/bin/clang-14 CXX=/usr/bin/clang++-14 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=.. ..
+CXXFLAGS="-stdlib=libc++" CC=/usr/bin/clang CXX=/usr/bin/clang++ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=.. ..
 cmake --build . --target install
